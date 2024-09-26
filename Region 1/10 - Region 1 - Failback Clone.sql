24c07a2ef6e1fca2316cb91ec390223c45d958b9
rhoubergdb <rhoubergdb>
2024-09-26 21:45:54 +0000

    update

--- Region 1/10 - Region 1 - Failback Clone.sql
+++ Region 1/10 - Region 1 - Failback Clone.sql
@@ -5,14 +5,29 @@
 
 -- COMMAND ----------
 
-CREATE OR REPLACE TABLE russh.dr_example.table1 DEEP CLONE delta.`abfss://russh-ext-location@russhdemosa.dfs.core.windows.net/table1`
-
--- COMMAND ----------
-
+-- DBTITLE 1,Get (OUTDATED!) History
 DESCRIBE HISTORY russh.dr_example.table1
 
 -- COMMAND ----------
 
+-- DBTITLE 1,Show Outdated Data (Missing Region 2 Update)
+SELECT 'original' as version,* FROM russh.dr_example.table1@v1 order by id;
+SELECT 'region 1 update' as version,* FROM russh.dr_example.table1@v2 order by id;
+SELECT 'region 2 update' as version,* FROM russh.dr_example.table1@v3 order by id;
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Update Table1 with Failback Clone Data
+CREATE OR REPLACE TABLE russh.dr_example.table1 DEEP CLONE delta.`abfss://russh-ext-location@russhdemosa.dfs.core.windows.net/table1`
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Show new CLONE entry in history
+DESCRIBE HISTORY russh.dr_example.table1
+
+-- COMMAND ----------
+
+-- DBTITLE 1,Show CURRENT data (Updated from Region 2)
 SELECT 'original' as version,* FROM russh.dr_example.table1@v1 order by id;
 SELECT 'region 1 update' as version,* FROM russh.dr_example.table1@v2 order by id;
 SELECT 'region 2 update' as version,* FROM russh.dr_example.table1@v3 order by id;
